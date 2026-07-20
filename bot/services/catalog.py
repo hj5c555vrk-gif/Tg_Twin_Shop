8f4a2e72ed568ada5dee44290c3a38e6dc400633
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-20 21:43:33 +0500

    Update catalog.py
    UPD 1

--- bot/services/catalog.py
+++ bot/services/catalog.py
@@ -1,6 +1,9 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from bot.database.models import Category
 
 
-async def get_categories(session):
+async def get_categories(session: AsyncSession):
     result = await session.execute(select(Category))
     return result.scalars().all()
