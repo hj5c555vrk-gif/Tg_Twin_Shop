46c71c5fb064d5e34e1ff701092e3f189a21dd46
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-20 21:30:12 +0500

    Update main.py
    upd 4

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 import asyncio
 import logging
+import os
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
-import os
+
+from bot.handlers.user import user_router
+from bot.database.seed_categories import seed_categories
+
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-from bot.handlers.user import user_router
-
 logging.basicConfig(level=logging.INFO)
 
+
 async def main():
-    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
     dp = Dispatcher()
     dp.include_router(user_router)
 
     # Создаём таблицы при запуске
     from bot.database.base import engine, async_session
     from bot.database.models import Base
+
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    # Добавляем категории
     async with async_session() as session:
         await seed_categories(session)
 
-print("Бот запущен с базой данных!")
+    print("Бот запущен с базой данных!")
+
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
