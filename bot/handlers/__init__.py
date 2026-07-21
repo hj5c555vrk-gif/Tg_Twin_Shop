f8869df8acc15bd704f3c9de8942b0ecea945757
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 12:42:44 +0500

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,8 +1,10 @@
 from bot.handlers.user import user_router
 from bot.handlers.catalog import catalog_router
+from bot.handlers.product import product_router
 
 
 routers = (
     user_router,
     catalog_router,
+    product_router,
 )
