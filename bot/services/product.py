cb9208d9b197d46502e51df046f14925f7791f73
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 19:57:15 +0500

    Update product.py

--- bot/services/product.py
+++ bot/services/product.py
@@ -13,3 +13,26 @@ async def get_products_by_category(
     )
 
     return result.scalars().all()
+    
+async def create_product(
+    session: AsyncSession,
+    name: str,
+    description: str,
+    price: int,
+    stock: int
+):
+
+    product = Product(
+        name=name,
+        description=description,
+        price=price,
+        stock=stock
+    )
+
+    session.add(product)
+
+    await session.commit()
+
+    await session.refresh(product)
+
+    return product
