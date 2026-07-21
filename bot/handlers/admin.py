8929e5104da0a0034fce9b8aa92b1311c0e4cc61
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 15:20:52 +0500

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -2,6 +2,8 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
 
+from bot.database.admin import ADMIN_ID
+
 
 admin_router = Router()
 
@@ -10,16 +12,21 @@ print("ADMIN ROUTER LOADED")
 
 
 @admin_router.message(Command("admin"))
-async def admin_panel(message: Message):
+async def admin_test(message: Message):
+
+    if message.from_user.id != ADMIN_ID:
+        await message.answer(
+            "⛔ У вас нет доступа к админ-панели"
+        )
+        return
+
 
     print(
         "ADMIN COMMAND RECEIVED:",
         message.from_user.id
     )
 
+
     await message.answer(
-        "🔐 Админ-панель\n\n"
-        "/stats — статистика\n"
-        "/users — пользователи\n"
-        "/products — товары"
+        "🔐 Доступ разрешён"
     )
