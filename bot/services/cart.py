bc6cba4c2e9f00f41118a4dd71bafc6a2cb415b8
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 13:05:14 +0500

    Update cart.py

--- bot/services/cart.py
+++ bot/services/cart.py
@@ -1,1 +1,52 @@
-#
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from bot.database.models import (
+    User,
+    Cart,
+    CartItem
+)
+
+
+async def get_or_create_cart(
+    session: AsyncSession,
+    telegram_id: int
+):
+
+    result = await session.execute(
+        select(User)
+        .where(
+            User.telegram_id == telegram_id
+        )
+    )
+
+    user = result.scalar()
+
+
+    if not user:
+        return None
+
+
+    result = await session.execute(
+        select(Cart)
+        .where(
+            Cart.user_id == user.id
+        )
+    )
+
+    cart = result.scalar()
+
+
+    if cart:
+        return cart
+
+
+    cart = Cart(
+        user_id=user.id
+    )
+
+    session.add(cart)
+
+    await session.commit()
+
+    return cart
