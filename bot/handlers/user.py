ca37861126732e30a55a02a8ecddb3645a60e346
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 10:53:49 +0500

    Update user.py
    upd del block //cat

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -11,24 +11,8 @@ user_router = Router()
 @user_router.message(Command("start"))
 async def cmd_start(message: Message):
     await message.answer(
-        "Добро пожаловать в магазин!\n\n"
-        "Команды:\n"
+        "Всем сап, это Twinstore!\n\n"
+        "Команды Навигации по магазину \n"
         "/catalog — посмотреть категории\n"
         "/start — перезапустить"
-    )
-@user_router.message(Command("catalog"))
-async def show_catalog(message: Message):
-    async with async_session() as session:
-        categories = await get_categories(session)
-
-    if not categories:
-        await message.answer(
-            "Пока нет категорий.\nДобавьте их позже через админ-панель."
-        )
-        return
-
-    await message.answer(
-        "<b>📦 Каталог товаров</b>\n\nВыберите категорию:",
-        reply_markup=catalog_keyboard(categories),
-        parse_mode="HTML"
     )
