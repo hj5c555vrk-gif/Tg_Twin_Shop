83dfec482f406dc08422c4bb6c5e4960e38c6579
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 15:40:51 +0500

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -3,7 +3,10 @@ from aiogram.types import Message
 from aiogram.filters import Command
 
 from bot.database.admin import ADMIN_ID
-from bot.keyboards.admin_key import admin_keyboard
+from bot.keyboards.admin_key import admin_keyboard(
+    admin_keyboard,
+    back_to_admin_keyboard,
+)
 
 
 admin_router = Router()
@@ -13,15 +16,20 @@ print("ADMIN ROUTER LOADED")
 
 
 @admin_router.message(Command("admin"))
-async def admin_test(message: Message):
+async def admin_panel(message: Message):
 
     if message.from_user.id != ADMIN_ID:
         await message.answer(
-            "⛔ У вас нет доступа к админ-панели"
+            "⛔ У вас нет доступа."
         )
         return
 
-
+    await message.answer(
+        "<b>🔐 Панель администратора</b>\n\n"
+        "Выберите необходимый раздел.",
+        reply_markup=admin_keyboard,
+        parse_mode="HTML",
+    )
     print(
         "ADMIN COMMAND RECEIVED:",
         message.from_user.id
@@ -36,15 +44,41 @@ async def admin_test(message: Message):
     parse_mode="HTML"
     )
     
-from aiogram.types import CallbackQuery
-from aiogram import F
+@admin_router.callback_query(
+    F.data.startswith("admin_"),
+    F.data != "admin_menu",
+)
+async def admin_stub(callback: CallbackQuery):
 
-
-@admin_router.callback_query(F.data.startswith("admin_"))
-async def admin_callbacks(callback: CallbackQuery):
+    if callback.from_user.id != ADMIN_ID:
+        await callback.answer(
+            "Нет доступа",
+            show_alert=True,
+        )
+        return
 
     await callback.answer()
 
     await callback.message.edit_text(
-        "🚧 Этот раздел находится в разработке."
+        "🚧 Этот раздел находится в разработке.",
+        reply_markup=back_to_admin_keyboard,
+    )
+    
+@admin_router.callback_query(F.data == "admin_menu")
+async def back_to_admin(callback: CallbackQuery):
+
+    if callback.from_user.id != ADMIN_ID:
+        await callback.answer(
+            "Нет доступа",
+            show_alert=True,
+        )
+        return
+
+    await callback.answer()
+
+    await callback.message.edit_text(
+        "<b>🔐 Панель администратора</b>\n\n"
+        "Выберите необходимый раздел.",
+        reply_markup=admin_keyboard,
+        parse_mode="HTML",
     )
