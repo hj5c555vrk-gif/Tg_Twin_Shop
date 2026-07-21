781bc4dafe4f28c1f4e265aa666f2699051f4ece
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 11:51:03 +0500

    Update models.py
    Big upd

--- bot/database/models.py
+++ bot/database/models.py
@@ -5,25 +5,92 @@ from bot.database.base import Base
 
 
 class Category(Base):
-    __tablename__ = 'categories'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False, unique=True)
+    __tablename__ = "categories"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    name = Column(
+        String(100),
+        nullable=False,
+        unique=True
+    )
+
+    products = relationship(
+        "Product",
+        back_populates="category"
+    )
+
 
 class Product(Base):
-    __tablename__ = 'products'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(200), nullable=False)
-    description = Column(Text)
-    price = Column(Float, nullable=False)
-    available = Column(Boolean, default=True)
-    image = Column(String(255))
-    category_id = Column(Integer, ForeignKey('categories.id'))
+    __tablename__ = "products"
 
-    flavors = relationship("Flavor", back_populates="product")
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    name = Column(
+        String(200),
+        nullable=False
+    )
+
+    description = Column(
+        Text
+    )
+
+    price = Column(
+        Float,
+        nullable=False
+    )
+
+    available = Column(
+        Boolean,
+        default=True
+    )
+
+    image = Column(
+        String(255)
+    )
+
+    category_id = Column(
+        Integer,
+        ForeignKey("categories.id")
+    )
+
+
+    category = relationship(
+        "Category",
+        back_populates="products"
+    )
+
+    flavors = relationship(
+        "Flavor",
+        back_populates="product"
+    )
+
 
 class Flavor(Base):
-    __tablename__ = 'flavors'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    product_id = Column(Integer, ForeignKey('products.id'))
-    product = relationship("Product", back_populates="flavors")
+    __tablename__ = "flavors"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    name = Column(
+        String(100),
+        nullable=False
+    )
+
+    product_id = Column(
+        Integer,
+        ForeignKey("products.id")
+    )
+
+    product = relationship(
+        "Product",
+        back_populates="flavors"
+    )
