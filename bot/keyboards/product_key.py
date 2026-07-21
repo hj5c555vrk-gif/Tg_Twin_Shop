e556d3f0464dcfc825d0bd2856707a9b43548774
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 10:48:02 +0500

    Update product_key.py
    Crat

--- bot/keyboards/product_key.py
+++ bot/keyboards/product_key.py
@@ -1,1 +1,28 @@
-#
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def products_keyboard(products):
+    keyboard = []
+
+    for product in products:
+        keyboard.append(
+            [
+                InlineKeyboardButton(
+                    text=product.name,
+                    callback_data=f"product_{product.id}"
+                )
+            ]
+        )
+
+    keyboard.append(
+        [
+            InlineKeyboardButton(
+                text="⬅️ Назад",
+                callback_data="back_to_catalog"
+            )
+        ]
+    )
+
+    return InlineKeyboardMarkup(
+        inline_keyboard=keyboard
+    )
