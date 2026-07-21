f39284ef64af31ccd2c2d2f3319054cf769fb66e
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 12:56:44 +0500

    Update user.py

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -5,14 +5,22 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from bot.database.base import async_session
 from bot.services.catalog import get_categories
 from bot.keyboards.catalog_key import catalog_keyboard
+from bot.services.user import get_or_create_user
 
 user_router = Router()
 
 @user_router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer(
-        "Всем сап, это Twinstore!\n\n"
-        "Команды Навигации по магазину \n"
-        "/catalog — посмотреть категории\n"
-        "/start — перезапустить"
+
+    async with async_session() as session:
+        await get_or_create_user(
+            session,
+            telegram_id=message.from_user.id,
+            username=message.from_user.username,
+            first_name=message.from_user.first_name
+        await message.answer(
+            "Сап 🖐️, это Twinstore!\n\n"
+            "🗺️Команды Навигации \n\n"
+            "/catalog — посмотреть категории\n"
+            "/start — перезапустить"
     )
