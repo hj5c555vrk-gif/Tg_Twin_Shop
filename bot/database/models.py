0918ce6e5d3a795f6d699a4b24a4d3849f1463fc
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 13:04:31 +0500

    Update models.py

--- bot/database/models.py
+++ bot/database/models.py
@@ -123,3 +123,54 @@ class User(Base):
         String(50),
         nullable=True
     )
+    
+class Cart(Base):
+    __tablename__ = "carts"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id")
+    )
+
+    items = relationship(
+        "CartItem",
+        back_populates="cart"
+    )
+
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    cart_id = Column(
+        Integer,
+        ForeignKey("carts.id")
+    )
+
+    product_id = Column(
+        Integer,
+        ForeignKey("products.id")
+    )
+
+    quantity = Column(
+        Integer,
+        default=1
+    )
+
+    cart = relationship(
+        "Cart",
+        back_populates="items"
+    )
+
+    product = relationship(
+        "Product"
+    )
