6b0a4b57dd2f94680c378f888f901adf1ade36bf
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 10:45:46 +0500

    Update product.py
    Create

--- bot/services/product.py
+++ bot/services/product.py
@@ -1,1 +1,15 @@
-#
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from bot.database.models import Product
+
+
+async def get_products_by_category(
+    session: AsyncSession,
+    category_id: int
+):
+    result = await session.execute(
+        select(Product).where(Product.category_id == category_id)
+    )
+
+    return result.scalars().all()
