99754477e95935591f4b2d5158aa7d4b0efb9c2e
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 21:00:36 +0500

    Update admin_states.py

--- bot/states/admin_states.py
+++ bot/states/admin_states.py
@@ -3,10 +3,25 @@ from aiogram.fsm.state import State, StatesGroup
 
 class AddProductStates(StatesGroup):
 
+    # Добавление товара
+
     name = State()
 
     description = State()
 
+    category = State()
+
     price = State()
 
     stock = State()
+
+
+    # Редактирование товара
+
+    edit_name = State()
+
+    edit_description = State()
+
+    edit_price = State()
+
+    edit_stock = State()
