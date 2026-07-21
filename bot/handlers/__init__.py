472ead9a55f392518de920125021c802727bea52
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-21 14:57:35 +0500

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,9 +1,12 @@
-print("HANDLERS INIT LOADED")
+print("HANDLERS INIT START")
 
 from bot.handlers.user import user_router
 from bot.handlers.catalog import catalog_router
 from bot.handlers.product import product_router
 from bot.handlers.admin import admin_router
+
+print("ADMIN ROUTER IMPORTED")
+
 
 routers = (
     user_router,
