d1f1d075ee827ab0c5d18cd8acd6230a8228ee9d
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-22 16:39:31 +0500

    Update menu.py

--- bot/handlers/menu.py
+++ bot/handlers/menu.py
@@ -20,3 +20,19 @@ async def open_user_menu(callback: CallbackQuery):
         "Выберите необходимый раздел.",
         reply_markup=user_menu_keyboard
     )
+    
+from bot.keyboards.admin_key import admin_keyboard
+
+
+@menu_router.callback_query(
+    F.data == "user_menu",
+    AdminFilter()
+)
+async def open_admin_menu(callback: CallbackQuery):
+
+    await callback.answer()
+
+    await callback.message.edit_text(
+        "🛠 Главное меню администратора",
+        reply_markup=admin_keyboard
+    )
