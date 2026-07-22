9e0fc1ee00efcb378ed2d5dcb737d87fbc85e069
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-22 16:38:08 +0500

    Create menu.py

---
+++ bot/handlers/menu.py
@@ -0,0 +1,22 @@
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
+
+from bot.filters.admin import AdminFilter
+from bot.keyboards.user_key import user_menu_keyboard
+
+menu_router = Router()
+
+
+@menu_router.callback_query(
+    F.data == "user_menu",
+    ~AdminFilter()
+)
+async def open_user_menu(callback: CallbackQuery):
+
+    await callback.answer()
+
+    await callback.message.edit_text(
+        "📋 Главное меню\n\n"
+        "Выберите необходимый раздел.",
+        reply_markup=user_menu_keyboard
+    )
