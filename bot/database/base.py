91e493f5b1b87b83946ac318b79b6fef0765dd26
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-22 11:21:16 +0500

    Update base.py

--- bot/database/base.py
+++ bot/database/base.py
@@ -1,14 +1,30 @@
+from pathlib import Path
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///shop.db")
+BASE_DIR = Path(__file__).resolve().parents[2]
+DB_PATH = BASE_DIR / "shop.db"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"sqlite+aiosqlite:///{DB_PATH}"
+)
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+)
+
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
+
 
 class Base(DeclarativeBase):
     pass
