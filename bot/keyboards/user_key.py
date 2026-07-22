19792159a65e15c12663e5e6d578ce9053daef46
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-22 16:37:30 +0500

    Update user_key.py

--- bot/keyboards/user_key.py
+++ bot/keyboards/user_key.py
@@ -11,3 +11,38 @@ start_keyboard = InlineKeyboardMarkup(
         ]
     ]
 )
+
+
+user_menu_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+
+        [
+            InlineKeyboardButton(
+                text="📦 Каталог",
+                callback_data="catalog"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="🛒 Корзина",
+                callback_data="cart"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="📦 Мои заказы",
+                callback_data="orders"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="ℹ️ Информация",
+                callback_data="info"
+            )
+        ]
+
+    ]
+)
