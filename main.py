67cb7b02588a339816172d97425e2511dc8891b6
hj5c555vrk-gif <hj5c555vrk@privaterelay.appleid.com>
2026-07-23 11:12:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,21 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.handlers import routers
 from bot.database.base import engine, async_session
-from bot.database.models import Base
 from bot.database.seed_categories import seed_categories
 from bot.database.seed_products import seed_products
 
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-logging.basicConfig(level=logging.INFO)
+BOT_TOKEN = os.getenv(
+    "BOT_TOKEN"
+)
+
+
+logging.basicConfig(
+    level=logging.INFO
+)
 
 
 async def main():
@@ -32,26 +37,38 @@ async def main():
         )
     )
 
+
     dp = Dispatcher(
         storage=MemoryStorage()
     )
 
+
     for router in routers:
         dp.include_router(router)
 
-    async with engine.begin() as conn:
-        await conn.run_sync(
-            Base.metadata.create_all
-        )
+
+    # Временный seed данных
+    # после подключения Alembic
+    # перенесем отдельно
 
     async with async_session() as session:
+
         await seed_categories(session)
+
         await seed_products(session)
 
-    print("Бот запущен с базой данных!")
 
-    await dp.start_polling(bot)
+    print(
+        "Бот запущен!"
+    )
+
+
+    await dp.start_polling(
+        bot
+    )
+
 
 
 if __name__ == "__main__":
+
     asyncio.run(main())
